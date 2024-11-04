f269ec55b1e5f7cc1074cf5451a9e606c4fe5532
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-11-05 01:02:26 +0200

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 
 ╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
-│ 📂 *BOT Name:* _XLICON-V2-MD_
-│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
-│ 👤 *Owner:* _Salman Ahmad_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
+│ 📂 *BOT Name:* _𝙸𝙱𝙱𝙾𝚃-V10-MD_
+│ 📝 *Description:* _I'm 𝙸𝙱𝙱𝙾𝚃-𝚇𝙼𝙳-𝚅10. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
+│ 👤 *Owner:* _𝚃𝚌𝚛𝚘𝚗𝚎𝚋 𝙷𝚊𝚌𝚔𝚡_
+│ 🌐 *𝙶𝚛𝚘𝚞𝚙:* https://chat.whatsapp.com/LqD6utmLF699JIW9R82O95
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
